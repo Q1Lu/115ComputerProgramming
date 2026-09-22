ea9741c6cd35d4e306367a298fbd7fe9704ddd22
Q1Lu <arthurx72189@gmail.com>
2026-09-22 11:54:51 +0800

    20260922_1

--- 20260922/ex2.c
+++ 20260922/ex2.c
@@ -2,8 +2,9 @@
 #include <math.h>
 int main () {
     int price = 239;
+    int total = price * 5;
 
-    printf("商品的單價是:");
-    printf("%d\n", price*5);
+    printf(" 商品的單價是:%d\n ", price);
+    printf(" 商品的總價是:%d\n ", total);
     return 0;
 }
